8fd016d4690bbeb036d964d3fc151fb8dfee822f
antecessor <moremohebian@gmail.com>
2020-02-22 16:56:35 -0600

    finishing motion synthetic generator based on Cartesian sampler

--- Utils/MotionUtils/GenerateMotion.py
+++ Utils/MotionUtils/GenerateMotion.py
@@ -16,28 +16,9 @@ t2Path = baseDir + "T2/"
 t2Images = os.listdir(t2Path)
 
 
-def euler2mat(euler):
-    """ Convert Euler Angles to Rotation Matrix. """
-    euler = np.asarray(euler, dtype=np.float64).transpose()
-    assert euler.shape[-1] == 3, "Invalid shaped euler {}".format(euler)
 
-    ai, aj, ak = -euler[..., 2], -euler[..., 1], -euler[..., 0]
-    si, sj, sk = np.sin(ai), np.sin(aj), np.sin(ak)
-    ci, cj, ck = np.cos(ai), np.cos(aj), np.cos(ak)
-    cc, cs = ci * ck, ci * sk
-    sc, ss = si * ck, si * sk
-
-    mat = np.empty(euler.shape[:-1] + (3, 3), dtype=np.float64)
-    mat[..., 2, 2] = cj * ck
-    mat[..., 2, 1] = sj * sc - cs
-    mat[..., 2, 0] = sj * cc + ss
-    mat[..., 1, 2] = cj * sk
-    mat[..., 1, 1] = sj * ss + cc
-    mat[..., 1, 0] = sj * cs - sc
-    mat[..., 0, 2] = -sj
-    mat[..., 0, 1] = cj * si
-    mat[..., 0, 0] = cj * ci
-    return mat
+def linearNormalization(values):
+    return (values - np.min(values)) / (np.max(values) - np.min(values))
 
 
 def linearNormalization(values):
@@ -102,5 +83,5 @@ for imageName in t1Images:
     imgStructure = nib.load(t1Path + imageName)
     voxelSize = imgStructure.header["pixdim"]
     data = imgStructure.get_fdata()
-    generateMotion(data, voxelSize[1:4], [1.2, 1.5, 1.4], [10, 20, 30])
+    generateMotion(data, voxelSize[1:4], maxDisplacementInMillimeter=[1.5, 1.5, 1.5], maxRotInDegree=[10, 20, 30])
     pass
